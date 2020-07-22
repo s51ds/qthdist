2f82625fc533e8a1af9cfed7ef014d704071c4d1
derganc <derganc@iskratel.si>
2020-07-22 10:49:16 +0200

    Distance and New QTH

--- geo/distance_test.go
+++ geo/distance_test.go
@@ -47,24 +47,25 @@ func TestDistance(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := DistanceLocator(tt.args.locatorA, tt.args.locatorB)
+			got, err := LocatorDistance(tt.args.locatorA, tt.args.locatorB)
 			if (err != nil) != tt.wantErr {
-				t.Errorf("DistanceLocator() error = %v, wantErr %v", err, tt.wantErr)
+				t.Errorf("LocatorDistance() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
 			if got != tt.want {
-				t.Errorf("DistanceLocator() = %v, want %v", got, tt.want)
+				t.Errorf("LocatorDistance() = %v, want %v", got, tt.want)
 			}
 		})
 	}
 }
 
 func TestDistanceQTH(t *testing.T) {
-	jn76to, _ := MakeQthFromLOC("JN76TO")
-	aa00aa, _ := MakeQthFromLOC("AA00AA")
+	jn76to, _ := NewQthFromLocator("JN76TO")
+	aa00aa, _ := NewQthFromLocator("AA00AA")
+
 	type args struct {
-		a QTH
-		b QTH
+		a *QTH
+		b *QTH
 	}
 	tests := []struct {
 		name string
@@ -75,7 +76,7 @@ func TestDistanceQTH(t *testing.T) {
 		{
 			name: "zero qth",
 			args: args{
-				a: QTH{
+				a: &QTH{
 					Loc: "",
 					LatLon: internal.LatLonDeg{
 						Lat: 0,
@@ -86,7 +87,7 @@ func TestDistanceQTH(t *testing.T) {
 						Lng: 0,
 					},
 				},
-				b: QTH{
+				b: &QTH{
 					Loc: "",
 					LatLon: internal.LatLonDeg{
 						Lat: 0,
@@ -113,7 +114,9 @@ func TestDistanceQTH(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := DistanceQTH(tt.args.a, tt.args.b); got != tt.want {
+			if got := tt.args.a.Distance(tt.args.b); got != tt.want {
+
+				//			if got := DistanceQTH(*tt.args.a, *tt.args.b); got != tt.want {
 				t.Errorf("DistanceQTH() = %v, want %v", got, tt.want)
 			}
 		})
@@ -121,11 +124,11 @@ func TestDistanceQTH(t *testing.T) {
 }
 
 func TestQTH_Distance(t *testing.T) {
-	jn76to, _ := MakeQthFromLOC("JN76TO")
-	aa00aa, _ := MakeQthFromLOC("AA00AA")
-	JN94FQ, _ := MakeQthFromLOC("JN94FQ")
-	KN22XS, _ := MakeQthFromLOC("KN22XS")
-	JN76TM, _ := MakeQthFromLOC("JN76TM")
+	jn76to, _ := NewQthFromLocator("JN76TO")
+	aa00aa, _ := NewQthFromLocator("AA00AA")
+	JN94FQ, _ := NewQthFromLocator("JN94FQ")
+	KN22XS, _ := NewQthFromLocator("KN22XS")
+	JN76TM, _ := NewQthFromLocator("JN76TM")
 
 	tests := []struct {
 		name string
@@ -135,32 +138,32 @@ func TestQTH_Distance(t *testing.T) {
 	}{
 		{
 			name: "jn76to",
-			a:    &jn76to,
-			b:    &jn76to,
+			a:    jn76to,
+			b:    jn76to,
 			want: 0,
 		},
 		{
 			name: "aa00aa",
-			a:    &aa00aa,
-			b:    &aa00aa,
+			a:    aa00aa,
+			b:    aa00aa,
 			want: 0,
 		},
 		{
 			name: "E74G",
-			a:    &jn76to,
-			b:    &JN94FQ,
+			a:    jn76to,
+			b:    JN94FQ,
 			want: 306.4447689992149,
 		},
 		{
 			name: "LZ9X",
-			a:    &jn76to,
-			b:    &KN22XS,
+			a:    jn76to,
+			b:    KN22XS,
 			want: 920.3974068704536,
 		},
 		{
 			name: "S52ME",
-			a:    &jn76to,
-			b:    &JN76TM,
+			a:    jn76to,
+			b:    JN76TM,
 			want: 9.266256686128344,
 		},
 	}
--- geo/qth.go
+++ geo/qth.go
@@ -9,108 +9,110 @@ import (
 	"strings"
 )
 
+// QTH represents Maidenhead QTH Locator and associated pair of latitude and longitude
+// Use function NewQthFromLocator or NewQthFromPosition to create QTH
 type QTH struct {
 	Loc    string             // Maidenhead QTH Locator
 	LatLon internal.LatLonDeg // LatLon represent a point as a pair of latitude and longitude degrees
 	LatLng s2.LatLng          // LatLng represents a point on the unit sphere as a pair of angles.
 }
 
-// MakeQthFromLOC returns QTH for qthLocator
-func MakeQthFromLOC(qthLocator string) (QTH, error) {
-	qthLocator = strings.ToUpper(qthLocator)
+// NewQthFromLocator returns QTH for locator
+func NewQthFromLocator(locator string) (*QTH, error) {
+	locator = strings.ToUpper(locator)
 	qth := QTH{}
-	switch len(qthLocator) {
+	switch len(locator) {
 	case 6:
 		{
-			if matched, _ := regexp.MatchString(`^[A-R]{2}[0-9]{2}[A-X]{2}$`, qthLocator); matched {
+			if matched, _ := regexp.MatchString(`^[A-R]{2}[0-9]{2}[A-X]{2}$`, locator); matched {
 				f := internal.FieldDecode(internal.LatLonChar{
-					LatChar: qthLocator[1],
-					LonChar: qthLocator[0],
+					LatChar: locator[1],
+					LonChar: locator[0],
 				})
 				s := internal.SquareDecode(internal.LatLonChar{
-					LatChar: qthLocator[3],
-					LonChar: qthLocator[2],
+					LatChar: locator[3],
+					LonChar: locator[2],
 				})
 				ss := internal.SubsquareDecode(internal.LatLonChar{
-					LatChar: qthLocator[5],
-					LonChar: qthLocator[4],
+					LatChar: locator[5],
+					LonChar: locator[4],
 				})
 				lat := f.Decoded.Lat + s.Decoded.Lat + ss.Decoded.Lat/60 + 0.02083333 // 1.25' / 60
 				lon := f.Decoded.Lon + s.Decoded.Lon + ss.Decoded.Lon/60 + 0.04166667 // 2.5' / 60
-				return QTH{
-					Loc:    qthLocator,
+				return &QTH{
+					Loc:    locator,
 					LatLon: internal.LatLonDeg{Lat: lat, Lon: lon},
 					LatLng: s2.LatLngFromDegrees(lat, lon),
 				}, nil
 			} else {
-				return qth, internal.IllegalLocatorError(qthLocator)
+				return &qth, internal.IllegalLocatorError(locator)
 			}
 		}
 	case 4:
 		{
-			if matched, _ := regexp.MatchString(`^[A-R]{2}[0-9]{2}$`, qthLocator); matched {
+			if matched, _ := regexp.MatchString(`^[A-R]{2}[0-9]{2}$`, locator); matched {
 				f := internal.FieldDecode(internal.LatLonChar{
-					LatChar: qthLocator[1],
-					LonChar: qthLocator[0],
+					LatChar: locator[1],
+					LonChar: locator[0],
 				})
 				s := internal.SquareDecode(internal.LatLonChar{
-					LatChar: qthLocator[3],
-					LonChar: qthLocator[2],
+					LatChar: locator[3],
+					LonChar: locator[2],
 				})
 				lat := f.Decoded.Lat + s.Decoded.Lat + 0.5
 				lon := f.Decoded.Lon + s.Decoded.Lon + 1
-				return QTH{
-					Loc:    qthLocator,
+				return &QTH{
+					Loc:    locator,
 					LatLon: internal.LatLonDeg{Lat: lat, Lon: lon},
 					LatLng: s2.LatLngFromDegrees(lat, lon),
 				}, nil
 			} else {
-				return qth, internal.IllegalLocatorError(qthLocator)
+				return &qth, internal.IllegalLocatorError(locator)
 			}
 		}
 	case 2:
 		{
-			if matched, _ := regexp.MatchString(`^[A-R]{2}`, qthLocator); matched {
+			if matched, _ := regexp.MatchString(`^[A-R]{2}`, locator); matched {
 				f := internal.FieldDecode(internal.LatLonChar{
-					LatChar: qthLocator[1],
-					LonChar: qthLocator[0],
+					LatChar: locator[1],
+					LonChar: locator[0],
 				})
 				lat := f.Decoded.Lat + 5
 				lon := f.Decoded.Lon + 10
 
-				return QTH{
-					Loc:    qthLocator,
+				return &QTH{
+					Loc:    locator,
 					LatLon: internal.LatLonDeg{Lat: lat, Lon: lon},
 					LatLng: s2.LatLngFromDegrees(lat, lon),
 				}, nil
 
 			} else {
-				return qth, internal.IllegalLocatorError(qthLocator)
+				return &qth, internal.IllegalLocatorError(locator)
 			}
 		}
 
 	default:
-		return qth, internal.IllegalLocatorError(qthLocator)
+		return &qth, internal.IllegalLocatorError(locator)
 	}
 }
 
-// MakeQthFromLatLon returns QTH for latitude, longitude
-func MakeQthFromLatLon(latitude, longitude float64) (QTH, error) {
+// NewQthFromPosition returns QTH for latitude, longitude
+func NewQthFromPosition(latitude, longitude float64) (*QTH, error) {
 	lld := internal.LatLonDeg{
 		Lat: latitude,
 		Lon: longitude,
 	}
 	if math.Abs(latitude) > 90 || math.Abs(longitude) > 180 {
-		return QTH{}, internal.IllegalLocatorError(lld.String())
+		return &QTH{}, internal.IllegalLocatorError(lld.String())
 	}
 	f, s, ss := internal.SubsquareEncode(lld)
-	return QTH{
+	return &QTH{
 		Loc:    f.Encoded.GetLonChar() + f.Encoded.GetLatChar() + s.Encoded.GetLonChar() + s.Encoded.GetLatChar() + ss.Encoded.GetLonChar() + ss.Encoded.GetLatChar(),
 		LatLon: lld,
 		LatLng: s2.LatLngFromDegrees(latitude, longitude),
 	}, nil
 }
 
-func (a QTH) String() string {
+func (a *QTH) String() string {
 	return fmt.Sprintf("[ %s %s {%.6f %.6f} ]", a.Loc, a.LatLon.String(), a.LatLng.Lat.Radians(), a.LatLng.Lng.Radians())
 }
